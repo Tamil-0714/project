17ba386efc9207b6371a9c3678c1d2410ac15257
Tamil-0714 <tom.jerry.07.05.06@gmail.com>
2025-06-14 10:33:40 +0530

    feat: add many features

--- Main.c
+++ Main.c
@@ -5,6 +5,10 @@ int main(int argc, char const *argv[])
     printf("Some new changesn");
     printf("From wrok ");
     printf("From personal ");
+    printf("Feature 1 ");
+    printf("Feature 2 ");
+    printf("Feature 3 ");
+    printf("Feature 4 ");
     
     return 0;
 }
